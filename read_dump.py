2c3763e4838a58566c0692db490ebd5b885fbd51
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-04-02 18:13:04 +0200

    a little more work trying to filter clouds.  Haven't yet successfully created a projection of cloud_id but pretty close!

--- read_dump.py
+++ read_dump.py
@@ -3,6 +3,7 @@ from astropy.io import ascii
 import yt
 from yt.units import parsec, Msun, gram, centimeter, second, Kelvin, kiloparsec
 import sklearn.cluster
+from yt.data_objects.particle_filters import add_particle_filter
 
 t = ascii.read('ZAA0400_small.ascii')
 
@@ -63,16 +64,32 @@ for ii in range(4):
         proj.set_cmap('all','cubehelix')
         proj.save("{1}_projection_{0}th_quarter.png".format(ii, direction))
 
+def cloud_filterer(cloud_id):
+    def cloud_filter(dummy, data):
+        return data[('all','cloud_id') == cloud_id]
+    return cloud_filter
+
 for cloud_id in np.unique(label):
     d = {x: data[x][data['cloud_id'] == cloud_id] for x in data}
+    #add_particle_filter("cloud{0}".format(cloud_id), function=cloud_filterer(cloud_id),
+    #                    filtered_type='all', requires=["particle_type"])
     ds = yt.load_particles(d,
                            periodicity=(False,False,False),
                            bbox=bbox,
                            **loading_params)
+    #ds.add_particle_filter("cloud{0}".format(cloud_id))
+    
     #cloud_region = ds.h.all_data().cut_region(["obj['cloud_id'] == {0}".format(cloud_id)])
 
     for direction in 'xyz':
-        proj = yt.ProjectionPlot(ds, direction, ('deposit','all_density'))
+        proj = yt.ProjectionPlot(ds, direction, ('deposit','all_density'))#, data_source=cloud_region)
+        proj.set_buff_size((1024, 1024))
         proj.set_zlim('all', 1e-10, 1e-1)
         proj.set_cmap('all','cubehelix')
         proj.save("{1}_projection_cloud{0}.png".format(cloud_id, direction))
+
+ds = yt.load_particles(data,
+                       periodicity=(False,False,False),
+                       bbox=bbox,
+                       **loading_params)
+# currently fails proj = yt.ProjectionPlot(ds, direction, ('all','cloud_id'))
